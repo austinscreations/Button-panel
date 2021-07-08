1a6f077ec68d69592ed7d7e72316390c752987b9
austinscreations <70245481+austinscreations@users.noreply.github.com>
2021-07-07 21:32:18 -0400

    Add files via upload
    trying to get button detection working - this code has an issue with call back - but the serial output within the library is showing correct button presses.

---
+++ code/v3_buttons_test_3/buttons.cpp
@@ -0,0 +1,163 @@
+/*
+ * buttons.cpp
+ * 
+ * A push button Arduino library capable of returning the number of
+ * consecutive button presses made in quick succession or if the 
+ * button was held down for a long time. 
+ * 
+ * Based on LSC_Button by ben.jones12@gmail.com, forked from mdButton library
+ * Based on mdPushButton by Michel Deslierres <sigmdel.ca/michel>
+ * 
+ */
+
+#include "Arduino.h"
+#include "buttons.h"
+
+
+#define VERSION 0x0002
+
+// If active = LOW (the default) then the input must be pulled high most of the time
+//   This can be done with the internal pullup (set pullup = true which is also the default value) 
+//   or an external pull up resistor say 4.7K + connected to the  GPIO pin and Vcc
+//
+// If active = HIGH then the input must be pulled low most of the time
+//   This can be done with the internal pulldown (set pullup = true which is also the default value) 
+//   or an external pull down resistor say 4.7K + connected to the  GPIO pin and Vcc
+
+mdPushButton::mdPushButton(){}
+
+void mdPushButton::setup(uint8_t pin, uint8_t active, bool useInternalPullResistor) {
+
+  _pin = pin;
+  _active = active;
+
+  int mode = INPUT;
+  if (useInternalPullResistor) {
+    if (active == LOW)
+      mode = INPUT_PULLUP;
+    else {
+      // mode = INPUT_PULLDOWN where defined
+      #if defined(INPUT_PULLDOWN) 
+        mode = INPUT_PULLDOWN;
+      #elif defined(ESP8266)
+        if (pin == 16) mode = INPUT_PULLDOWN_16;
+      #endif  
+    }
+  }
+
+   pinMode(_pin, mode);
+  _buttonState.data.state = AWAIT_PRESS;
+}
+
+//void mdPushButton::OnButtonClicked( callback_int cb ){_OnClick1 = cb;}
+
+void mdPushButton::onButtonPressed(buttonPressedCallback callback)
+{ 
+  _onButtonPressed = callback; 
+}
+
+void mdPushButton::process() 
+{
+  // Process each input to see what, if any, events have occured
+  uint8_t state = _update();
+
+  // Check if we have a callback to handle the press events
+//  if (_onButtonPressed) 
+//  {
+//    for (uint8_t i = 0; i < BUTTON_COUNT; i++)
+//    {
+      // Only interested in buttons with events to report
+      if (state != BUTTON_NO_STATE) {
+        Serial.println(state);                                           <<<<<<<<<<<<<<<<<<<<<<<<<<< // showing correct data but no callback
+        _onButtonPressed(0, state);
+      }
+//    }
+//  }
+}
+
+uint8_t mdPushButton::_update()
+{
+   
+  // Work out how long since our last update so we can increment the event times for each button
+  uint16_t delta = millis() - _lastUpdateTime;
+  _lastUpdateTime = millis();
+  
+    // Increment the event time for this button
+    _eventTime = _eventTime + delta;
+    
+    // AWAIT_PRESS
+    if (_buttonState.data.state == AWAIT_PRESS) 
+    {
+      _buttonState.data.clicks = 0;
+      if (digitalRead(_pin) == _active)
+      {
+        _buttonState.data.state = DEBOUNCE_PRESS;
+        _eventTime = 0;
+      }
+    }
+    // DEBOUNCE_PRESS
+    else if (_buttonState.data.state == DEBOUNCE_PRESS) 
+    {
+      if (_eventTime > DEBOUNCE_PRESS_TIME) 
+      {
+        _buttonState.data.state = AWAIT_RELEASE;
+        _eventTime = 0;
+      }
+    }
+    // AWAIT_RELEASE
+    else if (_buttonState.data.state == AWAIT_RELEASE) 
+    {
+      if (!(digitalRead(_pin) == _active)) 
+      {
+        _buttonState.data.state = DEBOUNCE_RELEASE;
+        _eventTime = 0;
+      }
+      else
+      {
+        if (_eventTime > HOLD_TIME)
+        {
+          _buttonState.data.clicks = BUTTON_HOLD_STATE ;
+          _eventTime = 0;
+//          Serial.println("i was held");
+//          state = BUTTON_HOLD_STATE;
+         return BUTTON_HOLD_STATE;
+        }
+      }
+    }
+    // DEBOUNCE_RELEASE
+    else if (_buttonState.data.state == DEBOUNCE_RELEASE) 
+    {
+      if (_eventTime > DEBOUNCE_RELEASE_TIME) 
+      {
+        if (_buttonState.data.clicks == BUTTON_HOLD_STATE ) 
+        {
+          _buttonState.data.state = AWAIT_PRESS;
+          return 0;
+        }
+          _buttonState.data.clicks = min(BUTTON_MAX_CLICKS, _buttonState.data.clicks + 1);
+//          _buttonState.data.clicks += 1;
+          _buttonState.data.state = AWAIT_MULTI_PRESS;
+//          Serial.println("i had multipress");
+          _eventTime = 0; 
+        }
+      }
+    
+    // AWAIT_MULTI_PRESS
+    else if (_buttonState.data.state == AWAIT_MULTI_PRESS) 
+    { 
+      if (digitalRead(_pin) == _active)
+      {
+        _buttonState.data.state = DEBOUNCE_PRESS;
+        _eventTime = 0;
+      } 
+      else if (_eventTime > MULTI_CLICK_TIME) 
+      {
+        _buttonState.data.state = AWAIT_PRESS;
+        return _buttonState.data.clicks;
+//          state = _buttonState.data.clicks;
+      }
+    }
+  
+
+  return 0;
+}
---
+++ code/v3_buttons_test_3/buttons.h
@@ -0,0 +1,102 @@
+/*
+ * buttons.h
+ * 
+ * A push button Arduino library capable of returning the number of
+ * consecutive button presses made in quick succession or if the 
+ * button was held down for a long time. 
+ * 
+ * Based on LSC_Button by ben.jones12@gmail.com, forked from mdButton library
+ * Based on mdPushButton by Michel Deslierres <sigmdel.ca/michel>
+ * 
+ */
+
+ #ifndef BUTTONS_H
+#define BUTTONS_H
+
+
+#include "Arduino.h"  // needed for platformIO
+
+// May need to define this macro in Arduino IDE if a value != 0 is desired
+#ifndef DEBUG_PUSH_BUTTON
+#define DEBUG_PUSH_BUTTON 0  // 0 - no debugging, 1 - printSetup() available, 2 adds state machine debugging
+#endif
+
+#define DEBOUNCE_PRESS_TIME      15  // delay to debounce the make part of the signal
+#define DEBOUNCE_RELEASE_TIME    30  // delay to debounce the break part of the signal
+#define MULTI_CLICK_TIME        250  // if 0, does not check for multiple button clicks
+#define HOLD_TIME               500  // minimum time of button press for mdButton.status() to return a -1 (long button press)
+                                             // all times in milliseconds
+
+// Special state values
+#define BUTTON_NO_STATE          0
+#define BUTTON_HOLD_STATE       15
+
+// Max number of clicks we support (won't report more even if button clicked more often)
+#define BUTTON_MAX_CLICKS        5
+
+// Callback type of handler such as buttonClicked(int clicks)
+//typedef void (*callback_int)(int);
+
+// Callback type of handler such as buttonClicked(uint8_t buttonid, int clicks);
+// the button id is the hopefully unique GPIO pin to which it is connected.
+//typedef void (*callback_int_int)(uint8_t, int);
+
+typedef void (*buttonPressedCallback)(uint8_t, uint8_t);
+
+enum buttonState_t { AWAIT_PRESS, DEBOUNCE_PRESS, AWAIT_RELEASE, DEBOUNCE_RELEASE, AWAIT_MULTI_PRESS };
+
+union buttonData_t
+{
+  uint8_t _data;
+  struct 
+  {
+    buttonState_t state  : 4;
+    uint8_t clicks : 4;
+  } data;
+};
+
+
+class mdPushButton {
+  public:
+    mdPushButton();  //constructor
+    
+    void setup(uint8_t pin, uint8_t active = LOW, bool useInternalPullResistor = true); // setup input pin
+//    void setup(uint8_t buttonnum, uint8_t pin, uint8_t active = LOW, bool useInternalPullResistor = true); // setup input pin
+    
+    // Current library version
+    int16_t version;
+
+    // status, number of clicks since last update
+    // -1 = button held, 0 = button not pressed, 1, 2, ... number of times button pressed
+//    int status();
+
+    void process();
+
+    // Set callback function to be called when the button has been pressed   
+//    void OnButtonClicked(callback_int); // only one push button, or using separate click handlers for each button
+
+    void onButtonPressed(buttonPressedCallback);
+
+   
+  private:
+    uint8_t _pin;    
+    uint8_t _active;
+
+    buttonPressedCallback _onButtonPressed;
+
+    uint32_t _lastUpdateTime;
+    
+    uint8_t _BUTTON_COUNT;   // number of buttons connected
+    buttonData_t _buttonState;
+
+    uint16_t _eventTime;
+  
+//    callback_int _OnClick1;
+
+    uint8_t _update();
+
+
+   
+};
+
+#endif
